1ab1e50d6c138991c4969e7684d4294d8cd79add
alchin2 <142945840+alchin2@users.noreply.github.com>
2024-01-16 15:50:35 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -2,7 +2,7 @@ public class GoogleBillboard {
 
     public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";
 
-    public static void main() {
+   public static void main(final String[] args) {
         int i = 0;
         String digits = e.substring(2, 12);
         double dNum = Double.parseDouble(digits);
